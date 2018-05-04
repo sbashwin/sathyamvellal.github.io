7f999e8de57e567e43cee5a0465c40884b6cca22
Sathyam Vellal <sathyam.vellal@gmail.com>
2018-05-03 18:56:12 -0700

    Fixing git urls

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo "Cloning deploy branch ..."
-git clone -b master "git@github.com:${GITHUB_TOKEN}/sathyamvellal/sathyamvellal.github.io" site
+git clone -b master "https://${GITHUB_TOKEN}@github.com/sathyamvellal/sathyamvellal.github.io" site
 
 if [ -d "site" ]; then
   bundle exec jeyll build
   cd site
   git add .
   git commit -m "Site update"
-  git push "git@github.com:${GITHUB_TOKEN}/sathyamvellal/sathyamvellal.github.io" master
+  git push "https://${GITHUB_TOKEN}@github.com/sathyamvellal/sathyamvellal.github.io" master
   cd ..
 else
   echo "Could not clone deploy branch ..."
