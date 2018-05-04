3d9add60d036342bfe124e50927dda6e634ff6ff
Sathyam Vellal <sathyam.vellal@gmail.com>
2018-05-03 19:37:30 -0700

    Typooooo

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ echo "Cloning deploy branch ..."
 git clone -b master "https://${GITHUB_TOKEN}@github.com/sathyamvellal/sathyamvellal.github.io.git" site
 
 if [ -d "site" ]; then
-  bundle exec jeyll build
+  bundle exec jekyll build
   cd site
   git add .
   git commit -m "Site update"
