570e8417b2b02410f13c65773129644e395e4519
Sathyam Vellal <sathyam.vellal@gmail.com>
2018-05-03 20:04:29 -0700

    Correcting deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 echo "Cloning deploy branch ..."
-git clone -b master "https://${GITHUB_TOKEN}@github.com/sathyamvellal/sathyamvellal.github.io" site
+git clone -b master "https://${GITHUB_TOKEN}@github.com/sathyamvellal/sathyamvellal.github.io.git" site
 
 if [ -d "site" ]; then
-  bundle exec jeyll build
+  echo "Installing bundle ..."
+  bundle install
+  echo "Running jekyll build ..."
+  bundle exec jekyll build
+  echo "Adding and committing changes ..."
   cd site
   git add .
   git commit -m "Site update"
-  git push "https://${GITHUB_TOKEN}@github.com/sathyamvellal/sathyamvellal.github.io" master
+  echo "Pushing changes upstream ..."
+  git push "https://${GITHUB_TOKEN}@github.com/sathyamvellal/sathyamvellal.github.io.git" master
   cd ..
+  echo "Successful deploy"
 else
   echo "Could not clone deploy branch ..."
   exit 1
